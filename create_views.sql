a27ad6ccacaf4e4846f65780c312359b6b913f1a
Tre Kirkman <trekirkman@gmail.com>
2019-02-16 18:21:12 -0800

    tracked create_views

---
+++ create_views.sql
@@ -0,0 +1,35 @@
+--
+-- Name: log_path; Type: VIEW; Schema: public; Owner: vagrant
+--
+
+CREATE VIEW log_path as
+    SELECT date_trunc('day', time) as day, 
+    count(*) as total 
+    FROM log 
+    GROUP BY day;
+
+GO
+
+--
+-- Name: t_logs; Type: VIEW; Schema: public; Owner: vagrant
+--
+
+CREATE VIEW t_logs as
+    SELECT date_trunc('day', time) as day, 
+    count(*) as total 
+    FROM log 
+    GROUP BY day;
+
+GO
+
+--
+-- Name: e_logs; Type: VIEW; Schema: public; Owner: vagrant
+--
+
+CREATE VIEW e_logs as
+    SELECT date_trunc('day', time) as day, 
+    count(*) as errors 
+    FROM log 
+    WHERE status like '4%' 
+    GROUP BY day;
+
